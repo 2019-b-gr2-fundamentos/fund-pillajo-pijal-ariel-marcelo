77b1c307159f22f20db1f926c8225c43e085ea02
ariel <ariel.pillajo.@epn.edu.ec>
2019-10-28 01:40:22 -0500

    ultimo

---
+++ DEBERES/Variables-Primitivas.cs
@@ -0,0 +1,20 @@
+namespace Variables{
+    class Program{
+        static void Main (string[] args){
+           
+           //Tipos de datos primitivos de c#
+           
+           byte a; // Entero sin signo
+           sbyte b; // Entero con signo
+           short c; // Entero Corto con signo
+           ushort d; // Entero corto sin signo
+           int e; // Entero medio sin signo
+           uint f; // Entero Medio sin signo
+           long g; // Entero largo con signo 
+           ulong e; // Entero largo sin signo
+           
+       }
+
+   } 
+}
+    
