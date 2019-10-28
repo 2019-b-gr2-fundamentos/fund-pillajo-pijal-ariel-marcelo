109842785a14fcaa4913ccfa0ed36ac49edf30f5
Ariel-Pillajo <ariel.pillajo@epn.edu.ec>
2019-10-28 16:03:45 -0500

    buig

---
+++ CARPETA/3/05-pront.ts
@@ -0,0 +1,14 @@
+const numeroUnoString = prompt("Ingresa el numero 1");
+const numeroDosString = prompt("Ingresa el numero 2");
+
+const numeroUno = Number(numeroUnoString );
+const numeroDos = prompt(numeroDosString);
+
+console.log( numeroDos  +   numeroUno);
+
+
+
+
+
+
+
