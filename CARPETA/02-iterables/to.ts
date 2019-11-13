b7349300432bb18527e6e476702acab069211e56
ariel <ariel.pillajo.@epn.edu.ec>
2019-11-12 22:05:10 -0500

    cambios

---
+++ CARPETA/02-iterables/to.ts
@@ -0,0 +1,29 @@
+if(EsMultiplode3){
+  
+    Compiladores = Compiladores + 1;
+    
+}else if(EsNumeroPar){ //if anidados significa que solo se cumplira uno de estos no ambos
+   
+    Algebra = Algebra +1;
+    
+
+}else if(EsNumeroImpar){
+
+    AnalisisSoxioeconomico = AnalisisSoxioeconomico + 1;
+   
+}else if(EsMultiplode5){
+
+    FundamentosDeProgramacion = FundamentosDeProgramacion + 1;
+
+}else if(EsMultiplode7){
+
+    Calculo = Calculo + 1;
+
+}else if(EsMultiplode11){
+
+     Fisica = Fisica + 1;
+
+}else if(EsMultiplode13){
+
+     Ingles = Ingles + 1;
+}
--- CARPETA/02-iterables/while.ts
+++ CARPETA/02-iterables/while.ts
@@ -52,6 +52,10 @@ let DeberesRealizados = 0;
 let AnalisisSoxioeconomico =0;
 let Algebra = 0;
 let Compiladores = 0;
+let FundamentosDeProgramacion = 0;
+let Calculo = 0;
+let Fisica = 0;
+let Ingles = 0;
 
 
 while(DeberesRealizados < Deberes){
@@ -60,25 +64,44 @@ while(DeberesRealizados < Deberes){
    let EsMultiplode3 = DeberesRealizados % 3 == 0;
    let EsNumeroPar = DeberesRealizados % 2 == 0;
    let EsNumeroImpar = DeberesRealizados % 2 != 0;
+   let EsMultiplode5 = DeberesRealizados % 5 == 0;
+   let EsMultiplode7 = DeberesRealizados % 7 == 0;
+   let EsMultiplode11 = DeberesRealizados % 11 == 0;
+   let EsMultiplode13 = DeberesRealizados % 13 == 0;
 
+   if(EsMultiplode13){
 
+    Ingles = Ingles + 1;
 
-    if(EsMultiplode3){
+    }else if(EsMultiplode11){
+
+      Fisica = Fisica + 1;
+
+    }else if(EsMultiplode7){
+
+      Calculo = Calculo + 1;
+
+    }else if(EsMultiplode5){
+
+         FundamentosDeProgramacion = FundamentosDeProgramacion + 1;
+
+    }else if(EsMultiplode3){
   
-         Compiladores = Compiladores + 1;
-         
+      Compiladores = Compiladores + 1;
+      
     }else if(EsNumeroPar){ //if anidados significa que solo se cumplira uno de estos no ambos
         
-         Algebra = Algebra +1;
-         
+      Algebra = Algebra +1;
+      
 
     }else if(EsNumeroImpar){
 
-         AnalisisSoxioeconomico = AnalisisSoxioeconomico + 1;
-        
-
+      AnalisisSoxioeconomico = AnalisisSoxioeconomico + 1;
+     
     }
 
+
+    
 
 } 
 
@@ -87,5 +110,9 @@ while(DeberesRealizados < Deberes){
 console.log(' Hice ' + AnalisisSoxioeconomico  + '  deberes de Analisis ');
 console.log(' Hice ' + Algebra + ' deberes de Algebra');
 console.log(' Hice ' + Compiladores + ' deberes de Compiladores ');
+console.log(' Hice ' + FundamentosDeProgramacion + ' deberes de Fundamentos de la Programacion');
+console.log(' Hice ' + Calculo + ' deberes de Calculo ');
+console.log(' Hice ' + Fisica + ' deberes de Fisica ');
+console.log(' Hice ' + Ingles + ' deberes de Ingles ');
 
 //template string
