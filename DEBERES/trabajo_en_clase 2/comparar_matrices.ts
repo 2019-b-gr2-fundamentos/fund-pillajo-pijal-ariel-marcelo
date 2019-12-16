c99b0bd029a2d14cb7b8d02db3b28662192f1990
Ariel-Pillajo <ariel.pillajo@epn.edu.ec>
2019-12-16 16:01:32 -0500

    16-12-2019

--- DEBERES/trabajo_en_clase 2/comparar_matrices.ts
+++ DEBERES/trabajo_en_clase 2/comparar_matrices.ts
@@ -20,6 +20,7 @@ do{
 console.log("La matriz tiene todos sus elementos iguales");
 
 */
+/*
 
 var matriz1 = [
     [1,2,3],
@@ -48,6 +49,7 @@ if(filasMatriz1 == filasMatriz2){
 
 
 }
+*/
 // sumar las matrices
 //intercambiar las diagonales
 /*
@@ -67,22 +69,140 @@ function compararMatriz(
 
 
 
+const arregloMatriz = [
+
+    [1,2],
+    [3,4,5],
+    [6,7,8],
+    [9],
+    1
+]
+
+function compararMatriz(
+    matrizUno: number[][],
+    matrizDos: number[][]
+): boolean {
+return true;
+}
+
+function obtenerPrimeraDimension(matrizUno: number[][]): number | false{
+    //Validaciones
+   const esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
+   if(esValido){
+
+    const primeraDimemnsionArreglo = matrizUno.length;
+    return primeraDimemnsionArreglo;
+
+   }else{
+       return false;
+   }
+
+}
+
+function obtenerSegundaDimension(matrizUno: number[][]): number | false{
+    const esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
+    let longitudActualMaxima = 0; // Auxiliar
+    let longitudActualMinima = -1;// Auxliar
+    for(let i = 0; i < matrizUno.length; i++){
+        const elementoActual = martrizUno[i]; // arreglo
+        const longitudActual = elementoActual.length // SEGUNDA DIMENSION
+        if(longitudActualMaxima < longitudActual)
+        {
+            longitudActualMaxima = longitudActual;
+
+        }
+        if(longitudActualMinima == -1)
+        {
+            longitudActualMinima = longitudActual;
+        }else{
+
+            if(longitudActual < longitudActualMinima){
+                longitudActualMinima = longitudActual;
+
+            }
+        }
+    }
+}
+
+
+function verificarTodosLosElementosDeUnArregloSonArreglo(
+    arreglo: any[]
+): boolean {
+
+    for(let i = 0; i < arreglo.length; i++)
+    {
+        const elementoActual = arreglo[i];
+        const esUnArreglo = typeof elementoActual == "object" &&
+        elementoActual.indexOf; // truty
+        if(!esUnArreglo){
+            return false;
+        }
+
+    }
+    return true;
+}
+
+function main(){
+    //comparar Matriz()
+}
+
+/*
+
+const arregloMatriz = [
+    [ 1 , 2 ],
+    [ 3 , 4 , 5 ],
+    [ 6 , 7 , 8 ]
+    [ 9 ]
+    1
+]
+
+
+
+función compararMatriz (
+    matrizUno :  número [] [],
+    matrizDos :  número [] []
+) :  booleano {
+ volver  verdadero ;
+}
+
+función obtenerPrimeraDimension ( matrizUno :  número [] []) :  número  |  falso {
+    // VALIDACIONES
+    const esValido =  verificarTodosLosElementosDeUnArregloSonArreglo ( matrizUno );
+    if ( esValido ) {
+        const primeraDimensionArreglo =  matrizUno . longitud ;
+        volver  primeraDimensionArreglo ;
+    } más {
+        devuelve  falso ;
+    }
+}
+
+función obtenerSegundaDimension ( matrizUno :  número [] []) :  número  |  falso {
+    
+}
+
+función verificarTodosLosElementosDeUnArregloSonArreglo (
+    arreglo :  any []
+) : booleano {
+    for ( let i =  0 ; i  <  arreglo . length ; i ++ ) {
+        const elementoActual =  arreglo [ i ];
+        const esUnArreglo =  typeof  elementoActual  ==  ' objeto '  &&
+        elementoActual . indexOf ; // Truty
+        if ( ! esUnArreglo ) {
+            devuelve  falso ;
+        }
+    }
+    volver  verdadero ;
+}
+
+
+función main () {
+    // compararMatriz ()
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
