071af8ac270bd32b27d6ff370aac51f5d9c9f683
Atari777 <ariel.pillajo@epn.edu.ec>
2020-01-09 15:45:38 -0500

    9-01-2020

--- CARPETA/05-proyecto/index.ts
+++ CARPETA/05-proyecto/index.ts
@@ -2,6 +2,6 @@
 //const SumaLibreria = require("ariel-milib/lib/suma");
 // importar todo el contenido con el nombre "SumaLibreria"
 //del paquete ariel-milib/lib/suma
-import * as  SumaLibreria from  "ariel-milib/lib/suma";
+import * as SumaLibreria from 'ariel-milib/lib/suma';
 const Respuesta = SumaLibreria(1,2);
 console.log("La respuesta es: " , Respuesta);
