073dde55688377459285ebd2e8b44444b91025cd
Atari777 <ariel.pillajo@epn.edu.ec>
2020-01-09 16:51:43 -0500

    9-1-2020

--- CARPETA/05-proyecto/index.js
+++ CARPETA/05-proyecto/index.js
@@ -1,19 +1,15 @@
+"use strict";
+exports.__esModule = true;
 //Node
 //const SumaLibreria = require("ariel-milib/lib/suma");
 // importar todo el contenido con el nombre "SumaLibreria"
 //del paquete ariel-milib/lib/suma
-import * as  SumaLibreria from  "ariel-milib/lib/suma";
-import * as  PiLibreria from  "ariel-milib/lib/pi";
-
-const Respuesta = SumaLibreria(1,2);
-console.log("La respuesta es: " , Respuesta);
-console.log("La constante pi es:" , PiLibreria);
-
-
-
-
+var SumaLibreria = require("ariel-milib/lib/suma");
+var PiLibreria = require("ariel-milib/lib/pi");
+var Respuesta = SumaLibreria(1, 2);
+console.log("La respuesta es: ", Respuesta);
+console.log("La constante pi es:", PiLibreria);
 /*
 bueno si tenemos varios modul.exports en un archibo
 nosotros sustituimos * as ----> {a} donde a es el nombre de uno de los module.exports
 */
-
