e94f429b7c994af8eb478bee24a2063ca30e951c
Atari777 <ariel.pillajo@epn.edu.ec>
2020-01-09 16:57:19 -0500

    9- 01-2020

--- CARPETA/05-proyecto/index.js
+++ CARPETA/05-proyecto/index.js
@@ -3,8 +3,8 @@ exports.__esModule = true;
 //Node
 //const SumaLibreria = require("ariel-milib/lib/suma");
 // importar todo el contenido con el nombre "SumaLibreria"
-//del paquete ariel-milib/lib/suma
-var SumaLibreria = require("ariel-milib/lib/suma")
+//del paquete ariel-milib/lib/sum
+var SumaLibreria = require("ariel-milib/lib/suma");
 var PiLibreria = require("ariel-milib/lib/pi");
 var Respuesta = SumaLibreria(1, 2);
 console.log("La respuesta es: ", Respuesta);
@@ -13,5 +13,3 @@ console.log("La constante pi es:", PiLibreria);
 bueno si tenemos varios modul.exports en un archibo
 nosotros sustituimos * as ----> {a} donde a es el nombre de uno de los module.exports
 */
-var Respuesta = SumaLibreria(1, 2);
-console.log("La respuesta es: ", Respuesta);
