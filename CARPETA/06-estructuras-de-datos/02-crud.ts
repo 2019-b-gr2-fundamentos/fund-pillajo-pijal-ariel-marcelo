1437b873ad2410f631f6ac07f02e40cb19883c2f
Ariel-Pillajo <ariel.pillajo@epn.edu.ec>
2020-01-20 15:46:50 -0500

    20-01-2020

--- CARPETA/06-estructuras-de-datos/02-crud.ts
+++ CARPETA/06-estructuras-de-datos/02-crud.ts
@@ -1,9 +1,16 @@
 // 02 - crud.ts create read update delete 
 //const prompts = require('prompts');
+
 import * as prompts from 'prompts'; //importar el paquete prompts
 function main(){
-    obtenerDatosAnimalPerrito();
-
+    // npm uninstall -g tsc -> Windows
+    // npm install -g typescript -> windows
+    // sudo npm uninstall -g tsc -> Linux Mac 
+    // sudo npm install -g typescript --> Linux Mac
+    //obtenerDatosAnimalPerrito();//AsincronsOnteber
+    obtenerDatosAnimalPerritoSincrona()// Sincrona
+    .then()
+    .catch();
 }
 
 function obtenerDatosAnimalPerrito(){
@@ -54,6 +61,36 @@ function obtenerDatosAnimalPerrito(){
            console.log('Error',error);
        }
    );
+   console.log('Fin');
+}
+
+import {RespuestaEdad} from './interfaces/repuesta-edad.interface.';
+import {RespuestaNombre} from "./interfaces/respuesta-nombre.interface";
+
+async function obtenerDatosAnimalPerritoSincrona(){// Sincrona
+                                                   // Asincro
+    console.log('Inicio'); 
+    const preguntas = [
+        {
+            type:'number',
+            name:'Edad',
+            message: 'Puedes darme tu nombre?'
+
+        },
+        {
+            type: 'text',
+            name: 'nombre',
+            message: 'Puedes darme tu Nombre?'
+        },
+        {
+            type: 'text',
+            name: 'cedule',
+            message: 'Puedes darme tu cedula'
+        }
+
+    ]
+   const RespuestaEdad  = await prompts(preguntas);
+   console.log('Repuesta', RespuestaEdad);
    console.log('Fin');
 }
 
