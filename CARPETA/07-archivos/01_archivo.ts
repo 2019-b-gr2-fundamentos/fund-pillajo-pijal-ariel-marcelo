cff97b264b4e6981d2e93f430bf438a70885c186
Atari77 <ariel.pillajo@epn.edu.ec>
2020-01-23 16:41:34 -0500

    23-01-2020

--- CARPETA/07-archivos/01_archivo.ts
+++ CARPETA/07-archivos/01_archivo.ts
@@ -1,15 +1,47 @@
 import { leerArchivo } from "./02-leer-archivo"
 import { escribirArchivo } from "./03-Escribir-Archivo";
+import { Estudiante } from "./interfaces/estudiante.interface";
+import * as prompts from 'prompts';
+// como vamos a trabajar con el pompt que convierte en promesas rabajamos con una funcion asincrona
+async function main(){
 
-function main(){
+    const arregloEstudiantes: Estudiante[] = [];
+    const arregloPreguntas =[
+        {
+            type: 'text',
+            name: 'nombre',
+            message: 'Ingresa Nombre'
+        }
+    ];
 
-    const TextoLeido = leerArchivo();
+    const respuestaEstudiante = prompts(arregloPreguntas);
+    console.log(respuestaEstudiante);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*  const TextoLeido = leerArchivo();
 
     const nuevoContenido = 'Temgo hambre :( \n';
     escribirArchivo('./ejemplo.txt','');
 
     
     console.log(TextoLeido + nuevoContenido);
+    */
 }
 
 main();
