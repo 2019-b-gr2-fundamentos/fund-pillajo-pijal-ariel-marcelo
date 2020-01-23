a64ac7784a9d33cf82cda419545251030ba9861a
Atari77 <ariel.pillajo@epn.edu.ec>
2020-01-23 17:58:05 -0500

    23-01-2020

--- CARPETA/07-archivos/01_archivo.js
+++ CARPETA/07-archivos/01_archivo.js
@@ -1,11 +1,127 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var _02_leer_archivo_1 = require("./02-leer-archivo");
-var _03_Escribir_Archivo_1 = require("./03-Escribir-Archivo");
+var prompts = require("prompts");
+// como vamos a trabajar con el pompt que convierte en promesas rabajamos con una funcion asincrona
 function main() {
-    var TextoLeido = _02_leer_archivo_1.leerArchivo();
-    var nuevoContenido = 'Temgo hambre :( \n';
-    _03_Escribir_Archivo_1.escribirArchivo('./ejemplo.txt', '');
-    console.log(TextoLeido + nuevoContenido);
+    return __awaiter(this, void 0, void 0, function () {
+        var contador, contenidoArchivo, arregloCargadoDeArchivo, arregloEstudiantes, arregloPreguntas, respuestaEstudiante, NuevoRegistro, respuestaEstudianteDos, NuevoRegistroUno, idABuscar, indiceEncontrado, nombreEditar, buscar, EstudianteEncontrado;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    contador = 1;
+                    contenidoArchivo = _02_leer_archivo_1.leerArchivo('./ejemplo.txt');
+                    console.log('contenidoArchivo', contenidoArchivo);
+                    arregloCargadoDeArchivo = JSON.parse(contenidoArchivo);
+                    arregloEstudiantes = arregloCargadoDeArchivo;
+                    arregloPreguntas = [
+                        {
+                            type: 'text',
+                            name: 'nombre',
+                            message: 'Ingresa Nombre'
+                        }
+                    ];
+                    return [4 /*yield*/, prompts(arregloPreguntas)];
+                case 1:
+                    respuestaEstudiante = _a.sent();
+                    NuevoRegistro = {
+                        id: contador,
+                        nombre: respuestaEstudiante.nombre
+                    };
+                    contador = contador + 1;
+                    arregloEstudiantes.push(NuevoRegistro);
+                    return [4 /*yield*/, prompts(arregloPreguntas)];
+                case 2:
+                    respuestaEstudianteDos = _a.sent();
+                    NuevoRegistroUno = {
+                        id: contador,
+                        nombre: respuestaEstudianteDos.nombre
+                    };
+                    contador = contador + 1;
+                    arregloEstudiantes.push(NuevoRegistroUno);
+                    console.log('Cual usuario quieres editar?');
+                    console.log(arregloEstudiantes);
+                    return [4 /*yield*/, prompts({
+                            type: 'number',
+                            name: 'id',
+                            message: 'ingresa el identificado del registro editar'
+                        })
+                        // esta funcion  acepta otra funcion como dato de entrada
+                    ];
+                case 3:
+                    idABuscar = _a.sent();
+                    indiceEncontrado = arregloEstudiantes.findIndex(// return CONICION
+                    function (valorActual, indice, arreglo) {
+                        console.log(valorActual);
+                        console.log(indice);
+                        console.log(arreglo);
+                        return valorActual.id == idABuscar.id; // No devuelbe el indice
+                    } // si encuentra devuelve el indice
+                    ) // no encuentra
+                    ;
+                    console.log('Indice encontrado', indiceEncontrado);
+                    return [4 /*yield*/, prompts({
+                            type: 'text',
+                            name: 'nombre',
+                            message: 'Ingresa el nuevo NOmbre'
+                        })];
+                case 4:
+                    nombreEditar = _a.sent();
+                    arregloEstudiantes[indiceEncontrado].nombre = nombreEditar.nombre;
+                    console.log(arregloEstudiantes);
+                    return [4 /*yield*/, prompts({
+                            type: 'text',
+                            name: 'nombre',
+                            message: 'Buscar por ID o por nombre'
+                        })];
+                case 5:
+                    buscar = _a.sent();
+                    EstudianteEncontrado = arregloEstudiantes
+                        .find(function (valorActual) {
+                        console.log(valorActual.nombre);
+                        console.log(buscar.nombre);
+                        return valorActual.nombre == buscar.nombre;
+                    });
+                    console.log(EstudianteEncontrado);
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
-main();
+main().then().catch();
--- CARPETA/07-archivos/01_archivo.ts
+++ CARPETA/07-archivos/01_archivo.ts
@@ -4,8 +4,13 @@ import { Estudiante } from "./interfaces/estudiante.interface";
 import * as prompts from 'prompts';
 // como vamos a trabajar con el pompt que convierte en promesas rabajamos con una funcion asincrona
 async function main(){
-
-    const arregloEstudiantes: Estudiante[] = [];
+let contador = 1;
+const contenidoArchivo = leerArchivo(
+    './ejemplo.txt'
+);
+    console.log('contenidoArchivo', contenidoArchivo);
+    const arregloCargadoDeArchivo = JSON.parse(contenidoArchivo);
+    const arregloEstudiantes: Estudiante[] = arregloCargadoDeArchivo;
     const arregloPreguntas =[
         {
             type: 'text',
@@ -14,17 +19,85 @@ async function main(){
         }
     ];
 
-    const respuestaEstudiante = prompts(arregloPreguntas);
-    console.log(respuestaEstudiante);
+    const respuestaEstudiante = await  prompts(arregloPreguntas);
+    
+    
+
+   const NuevoRegistro = {
+       id: contador,
+       nombre: respuestaEstudiante.nombre
+   };
+
+   contador = contador + 1;
+   arregloEstudiantes.push(NuevoRegistro);
+
+   const respuestaEstudianteDos = await prompts(arregloPreguntas);
+
+   const NuevoRegistroUno = {
+    id: contador,
+    nombre: respuestaEstudianteDos.nombre
+};
+contador = contador + 1;
+arregloEstudiantes.push(NuevoRegistroUno);
 
 
 
 
+console.log('Cual usuario quieres editar?');
+console.log(arregloEstudiantes);
+
+// Operadores !!! --> Reemplazar al !For
+const idABuscar = await prompts(
+    {
+        type: 'number',
+        name: 'id',
+        message: 'ingresa el identificado del registro editar'
+    }
+)
+
+// esta funcion  acepta otra funcion como dato de entrada
+const indiceEncontrado = arregloEstudiantes.findIndex( // return CONICION
+     function(valorActual, indice, arreglo){
 
 
+         console.log(valorActual);
+         console.log(indice);
+         console.log(arreglo);
+         return valorActual.id == idABuscar.id; // No devuelbe el indice
+     } // si encuentra devuelve el indice
+)      // no encuentra
 
+console.log('Indice encontrado', indiceEncontrado);
 
+const nombreEditar = await prompts(
+    {
+        type: 'text',
+        name: 'nombre',
+        message: 'Ingresa el nuevo NOmbre'
+    }
+);
 
+arregloEstudiantes[indiceEncontrado].nombre = nombreEditar.nombre;
+console.log(arregloEstudiantes);
+
+const buscar = await prompts(
+    {
+        type: 'text',
+        name: 'nombre',
+        message: 'Buscar por ID o por nombre'
+    }
+);
+
+const EstudianteEncontrado = arregloEstudiantes
+    .find(
+         function(valorActual){
+             console.log(valorActual.nombre)
+             console.log(buscar.nombre)
+         return valorActual.nombre == buscar.nombre;
+        }
+    );
+
+    console.log(EstudianteEncontrado);
 
 
 
@@ -44,4 +117,4 @@ async function main(){
     */
 }
 
-main();
+main().then().catch();
